0ff20e1baaf827c6897ff812f0a422da3b4331ae
Atsushi Miyazaki <ki929.oa95cp@gmail.com>
2023-05-16 15:38:21 +0900

    add tests

--- module/add_test.pl
+++ module/add_test.pl
@@ -6,5 +6,11 @@
   test(invalid_array) :- not(add_all(['a', 'b', 2], _)).
 :- end_tests(add_all).
 
+:- begin_tests(add_some).
+  test(basecase) :- add_some([1,2,3,4], <(2), 7).
+  test(basecase2) :- add_some([1,2,3], <(0), 6).
+  test(invalid_array) :- not(add_some(['a', 'b', 2], _, _)).
+:- end_tests(add_some).
+
 :- run_tests.
 :- halt.
